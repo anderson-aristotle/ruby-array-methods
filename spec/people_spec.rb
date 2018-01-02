f1d8aeb8c3aedbf737f7d56d74f879e8e53867c6
caleb-pearce <caleb.pearce@generalassemb.ly>
2018-01-02 12:50:23 -0500

    Update tests to reflect changes to data
    Rerunning the `generate_people.rb` script to remove gender changed some
    of the data in `people.csv`, probably because the version of
    the text files in `census` that were used to create `people.csv`
    originally are different from what's in `census` now.
    
    This commit updates the test so that they're looking for the correct
    average data for this version of `people.csv`.

--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -17,25 +17,25 @@ describe 'ArrayMethods::Labs::People' do
 
   describe '#people_older_than(age)' do
     it 'returns the count of people older than (age)' do
-      expect(people.people_older_than(age)).to eq 1683
+      expect(people.people_older_than(age)).to eq 1613
     end
   end
 
   describe '#people_younger_than(age)' do
     it 'returns the count of people older than (age)' do
-      expect(people.people_younger_than(age)).to eq 724
+      expect(people.people_younger_than(age)).to eq 785
     end
   end
 
   describe '#people_with_same_first_last_start_letter' do
     it 'returns count of people with same first and last name start letter' do
-      expect(people.people_with_same_first_last_start_letter).to eq 132
+      expect(people.people_with_same_first_last_start_letter).to eq 127
     end
   end
 
   describe '#average_age' do
     it 'returns average age of people' do
-      expect(people.average_age).to eq 39
+      expect(people.average_age).to eq 38
     end
   end
 end
