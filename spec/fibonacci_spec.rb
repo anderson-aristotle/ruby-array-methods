9c7022cd46fc107863f92363d6eac96e916ae509
Danny Kirschner <rkirschner1377@gmail.com>
2018-02-15 11:44:51 -0500

    Refactor people and fibonacci to not use modules and classes
    Close #43
    Close #42

--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -1,31 +1,59 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'fibonacci'
 
-describe 'ArrayMethods::Labs::Fibonacci' do
-  let(:fibonacci) { ArrayMethods::Labs::Fibonacci.new }
-  let(:fibs) { [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] }
-
-  it "returns the first 10 numbers of the fibonacci sequence' do" do
-    expect(fibonacci.fibs).to eq fibs
-  end
-
-  describe '#product_of_elements' do
-    it 'calculates the product of elements within fibs, excluding 0' do
-      expect(fibonacci.product_of_elements).to eq 2_227_680
-    end
-  end
-
-  describe '#sum_of_odd_elements' do
-    it 'sums the odd elements within fibs' do
-      expect(fibonacci.sum_of_odd_elements).to eq 44
-    end
-  end
-
-  describe '#product_of_even_elements' do
-    it 'calculates the product of even elements within fibs' do
-      expect(fibonacci.product_of_even_elements).to eq 544
-    end
+describe 'fibonacci' do
+  it 'prints the results of the various fibonacci methods' do
+    results = <<-HEREDOC
+2227680
+44
+544
+    HEREDOC
+    expect{
+      require 'fibonacci'
+    }.to output(results).to_stdout
   end
 end
+
+## Example tests using class Fibonacci
+## needs fibonacci.rb to be rewritten in modular fashion,
+## for example if it were written as:
+#
+# class Fibonacci
+#
+# def initialize
+#   @fibonacci = [...]
+# end
+#
+# def product_of_elements
+#   @fibonacci.reduce {|el| ...}
+# end
+# 
+# etc ...
+#
+# describe 'fibonacci.rb' do
+#   let(:fibonacci) { ArrayMethods::Labs::Fibonacci.new }
+#   let(:fibs) { [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] }
+# 
+#   it "returns the first 10 numbers of the fibonacci sequence' do" do
+#     expect(fibonacci.fibs).to eq fibs
+#   end
+# 
+#   describe '#product_of_elements' do
+#     it 'calculates the product of elements within fibs, excluding 0' do
+#       expect(fibonacci.product_of_elements).to eq 2_227_680
+#     end
+#   end
+# 
+#   describe '#sum_of_odd_elements' do
+#     it 'sums the odd elements within fibs' do
+#       expect(fibonacci.sum_of_odd_elements).to eq 44
+#     end
+#   end
+# 
+#   describe '#product_of_even_elements' do
+#     it 'calculates the product of even elements within fibs' do
+#       expect(fibonacci.product_of_even_elements).to eq 544
+#     end
+#   end
+# end
--- spec/people_spec.rb
+++ spec/people_spec.rb
@@ -1,41 +1,70 @@
 # frozen_string_literal: true
 
 require 'spec_helper'
-require 'people'
 
-describe 'ArrayMethods::Labs::People' do
-  let(:people) { ArrayMethods::Labs::People.new }
-  let(:age) { 25 }
-
-  it 'collects Person objects from data/people.csv' do
-    expect(people.people.count).to eq 2438
-  end
-
-  it 'contains an array of Person objects' do
-    expect(people.people.sample).to be_an_instance_of Person
-  end
-
-  describe '#people_older_than(age)' do
-    it 'returns the count of people older than (age)' do
-      expect(people.people_older_than(age)).to eq 1613
-    end
-  end
-
-  describe '#people_younger_than(age)' do
-    it 'returns the count of people older than (age)' do
-      expect(people.people_younger_than(age)).to eq 785
-    end
-  end
-
-  describe '#people_with_same_first_last_start_letter' do
-    it 'returns count of people with same first and last name start letter' do
-      expect(people.people_with_same_first_last_start_letter).to eq 127
-    end
-  end
-
-  describe '#average_age' do
-    it 'returns average age of people' do
-      expect(people.average_age).to eq 38
-    end
+describe 'people' do
+  it 'prints the results of the various methods' do
+    results = <<-HEREDOC 
+2438
+1621
+780
+127
+38
+    HEREDOC
+    expect{
+      require 'people'
+    }.to output(results).to_stdout
   end
 end
+
+## Example tests using class People
+## needs people.rb to be rewritten in modular fashion, 
+## for example if it were written as:
+#
+# class People
+#
+# def initialize
+#   ....
+# end
+#
+# def people_older_than(age)
+#   ....
+# end
+#
+# etc ...
+#describe 'ArrayMethods::Labs::People' do
+#  let(:people) { ArrayMethods::Labs::People.new }
+#  let(:age) { 25 }
+#
+#  it 'collects Person objects from data/people.csv' do
+#    expect(people.people.count).to eq 2438
+#  end
+#
+#  it 'contains an array of Person objects' do
+#    expect(people.people.sample).to be_an_instance_of Person
+#  end
+#
+#  describe '#people_older_than(age)' do
+#    it 'returns the count of people older than (age)' do
+#      expect(people.people_older_than(age)).to eq 1613
+#    end
+#  end
+#
+#  describe '#people_younger_than(age)' do
+#    it 'returns the count of people older than (age)' do
+#      expect(people.people_younger_than(age)).to eq 785
+#    end
+#  end
+#
+#  describe '#people_with_same_first_last_start_letter' do
+#    it 'returns count of people with same first and last name start letter' do
+#      expect(people.people_with_same_first_last_start_letter).to eq 127
+#    end
+#  end
+#
+#  describe '#average_age' do
+#    it 'returns average age of people' do
+#      expect(people.average_age).to eq 38
+#    end
+#  end
+#end
