dcb1235f964ee2a3123cdf61771c3ec4a1172d25
cpearce31 <cpearce31@gmail.com>
2018-06-08 11:10:55 -0400

    Improve tests
    Instead of testing what gets written to stdout, the test now call the methods
    defined during the excercise and then examine the output.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -1,19 +1,18 @@
 # frozen_string_literal: true
 
-@fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
+fibs = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34]
 
-def product_of_elements
+def product_of_elements(numbers)
   # your code here
 end
 
-def product_of_even_elements
+def product_of_even_elements(numbers)
   # your code here
 end
 
-def sum_of_odd_elements
+def sum_of_odd_elements(numbers)
   # your code here
 end
 
-puts product_of_elements
-puts product_of_even_elements
-puts sum_of_odd_elements
+# You can test your code like this:
+puts product_of_elements(fibs)
--- spec/fibonacci_spec.rb
+++ spec/fibonacci_spec.rb
@@ -2,58 +2,26 @@
 
 require 'spec_helper'
 
-describe 'fibonacci' do
-  it 'prints the results of the various fibonacci methods' do
-    results = <<~HEREDOC
-      2227680
-      44
-      544
-    HEREDOC
-    expect do
-      require 'fibonacci'
-    end.to output(results).to_stdout
+describe 'fibonacci.rb' do
+  require_relative('../lib/fibonacci.rb')
+
+  let(:fibs) { [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] }
+
+  describe '#product_of_elements' do
+    it 'calculates the product of elements within fibs, excluding 0' do
+      expect(product_of_elements(fibs)).to eq 2_227_680
+    end
+  end
+
+  describe '#sum_of_odd_elements' do
+    it 'sums the odd elements within fibs' do
+      expect(sum_of_odd_elements(fibs)).to eq 44
+    end
+  end
+
+  describe '#product_of_even_elements' do
+    it 'calculates the product of even elements within fibs' do
+      expect(product_of_even_elements(fibs)).to eq 544
+    end
   end
 end
-
-## Example tests using class Fibonacci
-## needs fibonacci.rb to be rewritten in modular fashion,
-## for example if it were written as:
-#
-# class Fibonacci
-#
-# def initialize
-#   @fibonacci = [...]
-# end
-#
-# def product_of_elements
-#   @fibonacci.reduce {|el| ...}
-# end
-#
-# etc ...
-#
-# describe 'fibonacci.rb' do
-#   let(:fibonacci) { ArrayMethods::Labs::Fibonacci.new }
-#   let(:fibs) { [0, 1, 1, 2, 3, 5, 8, 13, 21, 34] }
-#
-#   it "returns the first 10 numbers of the fibonacci sequence' do" do
-#     expect(fibonacci.fibs).to eq fibs
-#   end
-#
-#   describe '#product_of_elements' do
-#     it 'calculates the product of elements within fibs, excluding 0' do
-#       expect(fibonacci.product_of_elements).to eq 2_227_680
-#     end
-#   end
-#
-#   describe '#sum_of_odd_elements' do
-#     it 'sums the odd elements within fibs' do
-#       expect(fibonacci.sum_of_odd_elements).to eq 44
-#     end
-#   end
-#
-#   describe '#product_of_even_elements' do
-#     it 'calculates the product of even elements within fibs' do
-#       expect(fibonacci.product_of_even_elements).to eq 544
-#     end
-#   end
-# end
