12c2bb337f5e8226f35a89276724636d891aeeee
cpearce31 <cpearce31@gmail.com>
2018-06-08 11:39:20 -0400

    Add some starter code for `lib/set_etc.rb`
    Give them arrays that have some overlap right off the bat to make the purpose
    of those array operators more obvious.

---
+++ lib/sets_etc.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+# Let's expirement with Ruby's array union, difference, and Intersection
+# operators. We'll try flattening a nested array, too. Use these arrays:
+
+delicious_foods = ['Pizza', 'Bananas', 'Chicken and Rice Guys', 'Kale']
+healthy_foods = ['Kale', 'Flax Seed', 'Bananas', 'Cauliflower']
