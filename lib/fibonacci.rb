87e7bbdc70624dddfb63ab46d06710260e057310
jordanallain <jordan.allain@generalassemb.ly>
2018-08-07 16:27:13 -0400

    Reorder methods to match instructions
    - Order the methods the same as the instructions and the specs.
    - Closes #47.

--- lib/fibonacci.rb
+++ lib/fibonacci.rb
@@ -6,11 +6,11 @@ def product_of_elements(numbers)
   # your code here
 end
 
-def product_of_even_elements(numbers)
+def sum_of_odd_elements(numbers)
   # your code here
 end
 
-def sum_of_odd_elements(numbers)
+def product_of_even_elements(numbers)
   # your code here
 end
 
